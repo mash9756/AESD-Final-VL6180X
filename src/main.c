3d0ee03e946e79a063283182508bea4b07d3f4d3
admin <admin@MARK-PC-VM.myguest.virtualbox.org>
2023-04-28 10:29:57 -0400

    Running main exited immediately removed LCD, add debug print

--- src/main.c
+++ src/main.c
@@ -16,10 +16,12 @@
 
 int main()
 {
+	printf("\nRunning VL6180x Main...");
 	vl6180 handle = vl6180_initialise(1);
 
 	if(handle<=0)
 	{
+		printf("\nNo VL6180x handle.");
 		return 1;
 	}
 
@@ -27,22 +29,22 @@ int main()
 	sleep(1);
 
 	int als 	= 0;
-	int bytes 	= 0;
-	char lineOne[LINE_LEN];
+	//int bytes 	= 0;
+	//char lineOne[LINE_LEN];
 	
-	int lcd = open(LCD_CHAR_DRIVER, O_RDWR|O_CREAT|O_APPEND, S_IRWXU|S_IRWXG|S_IRWXO);
+	//int lcd = open(LCD_CHAR_DRIVER, O_RDWR|O_CREAT|O_APPEND, S_IRWXU|S_IRWXG|S_IRWXO);
 
 	while(1)
 	{
 		als = get_ALS(handle);
 		printf("\nAmbient Light Value: %d", als);
 		printf("\nPrinting to LCD");
-		bytes = snprintf(lineOne, LINE_LEN, "ALS: %d Lux", als);
-		write(lcd, lineOne, LINE_LEN);
-		printf("\nWrote %d bytes to LCD", bytes);
-		sleep(5);
-		printf("\nClearing LCD");
-		ioctl(lcd, LCDCHAR_IOCCLEAR);
+		//bytes = snprintf(lineOne, LINE_LEN, "ALS: %d Lux", als);
+		//write(lcd, lineOne, LINE_LEN);
+		//printf("\nWrote %d bytes to LCD", bytes);
+		sleep(1);
+		//printf("\nClearing LCD");
+		//ioctl(lcd, LCDCHAR_IOCCLEAR);
 	}
 
 	return 0;
